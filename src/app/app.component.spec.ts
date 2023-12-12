6aecf4806ffbcc41108cab19e53e6d6d2e615899
Matt <matthew.schultz1992@gmail.com>
2023-12-12 23:19:30 +0000

    Added some unit tests for user-login form

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,39 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { AppComponent } from './app.component'
+import { FormsModule } from '@angular/forms'
+import { UserLoginComponent } from './user-login/user-login.component'
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+				FormsModule
+			],
+			declarations: [
+				AppComponent,
+				UserLoginComponent
+			],
+		}).compileComponents()
+	})
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+	it('should create the app', () => {
+		const fixture = TestBed.createComponent(AppComponent)
+		const app = fixture.componentInstance
+		expect(app).toBeTruthy()
+	})
 
-  it(`should have as title 'angular-coding-challenge'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-coding-challenge');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-coding-challenge app is running!');
-  });
-});
+	it(`should have as title 'angular-coding-challenge'`, () => {
+		const fixture = TestBed.createComponent(AppComponent)
+		const app = fixture.componentInstance
+		expect(app.title).toEqual('angular-coding-challenge')
+	})
+	
+	it('should contain text "Home Page', () => {
+		const fixture = TestBed.createComponent(AppComponent)
+		fixture.detectChanges()
+		const heading = fixture.nativeElement as HTMLElement
+		expect(heading.querySelector('h1')?.textContent).toContain('Home Page')
+	})
+})
--- src/app/user-login/user-login.component.spec.ts
+++ src/app/user-login/user-login.component.spec.ts
@@ -1,23 +1,91 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { UserLoginComponent } from './user-login.component';
+import { UserLoginComponent } from './user-login.component'
+import { FormsModule } from '@angular/forms'
+import { By } from '@angular/platform-browser'
 
 describe('UserLoginComponent', () => {
-	let component: UserLoginComponent;
-	let fixture: ComponentFixture<UserLoginComponent>;
+	let component: UserLoginComponent
+	let fixture: ComponentFixture<UserLoginComponent>
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-		declarations: [ UserLoginComponent ]
+			imports: [ FormsModule ],
+			declarations: [ UserLoginComponent ]
 		})
-		.compileComponents();
+		.compileComponents()
 
-		fixture = TestBed.createComponent(UserLoginComponent);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
-	});
+		fixture = TestBed.createComponent(UserLoginComponent)
+		component = fixture.componentInstance
+
+		fixture.detectChanges()
+	})
 
 	it('should create', () => {
-		expect(component).toBeTruthy();
-	});
-});
+		expect(component).toBeTruthy()
+	})
+
+	it('should disable the submit button when the username is empty', () => {
+		const username = fixture.debugElement.query(By.css('#username'))
+    	const password = fixture.debugElement.query(By.css('#password'))
+
+		username.nativeElement.value = ''
+    	username.nativeElement.dispatchEvent(new Event('input'))
+
+		password.nativeElement.value = 'Password1234'
+    	password.nativeElement.dispatchEvent(new Event('input'))
+
+		fixture.detectChanges()
+	
+		const submitButton: HTMLButtonElement = fixture.nativeElement.querySelector('button')
+		expect(submitButton.disabled).toBeTruthy()
+	})
+
+	it('should disable the submit button when the password is empty', () => {
+		const username = fixture.debugElement.query(By.css('#username'))
+    	const password = fixture.debugElement.query(By.css('#password'))
+
+		username.nativeElement.value = 'CoolUsername'
+    	username.nativeElement.dispatchEvent(new Event('input'))
+
+		password.nativeElement.value = ''
+    	password.nativeElement.dispatchEvent(new Event('input'))
+
+		fixture.detectChanges()
+	
+		const submitButton: HTMLButtonElement = fixture.nativeElement.querySelector('button')
+		expect(submitButton.disabled).toBeTruthy()
+	})
+
+	it('should disable the submit button when the password does not meet the requirements', () => {
+		const username = fixture.debugElement.query(By.css('#username'))
+    	const password = fixture.debugElement.query(By.css('#password'))
+
+		username.nativeElement.value = 'CoolUsername'
+    	username.nativeElement.dispatchEvent(new Event('input'))
+
+		password.nativeElement.value = 'password'
+    	password.nativeElement.dispatchEvent(new Event('input'))
+
+		fixture.detectChanges()
+	
+		const submitButton: HTMLButtonElement = fixture.nativeElement.querySelector('button')
+		expect(submitButton.disabled).toBeTruthy()
+	})
+
+	it('should enable the submit button whith a valid username and password', () => {
+		const username = fixture.debugElement.query(By.css('#username'))
+    	const password = fixture.debugElement.query(By.css('#password'))
+
+		username.nativeElement.value = 'CoolUsername'
+    	username.nativeElement.dispatchEvent(new Event('input'))
+
+		password.nativeElement.value = 'Password1234'
+    	password.nativeElement.dispatchEvent(new Event('input'))
+
+		fixture.detectChanges()
+	
+		const submitButton: HTMLButtonElement = fixture.nativeElement.querySelector('button')
+		expect(submitButton.disabled).toBeFalsy()
+	})
+})
